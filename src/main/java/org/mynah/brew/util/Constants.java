022f486d736c26fef9d6c69cc399b1e26370cf9f
Lamb <lambgao@me.com>
2013-01-17 17:13:26 +0800

    重命名配置文件名

--- src/main/java/org/mynah/brew/util/Constants.java
+++ src/main/java/org/mynah/brew/util/Constants.java
@@ -2,7 +2,7 @@ package org.mynah.brew.util;
 
 public final class Constants {
 
-    public static final String PROJECT_NAME = "Mist";
+    public static final String PROJECT_NAME = "Brew";
     public static final String COOKIE_SU = "SU";
     public static final String SESSION_USER = "user";
     public static final int HUNDRED = 100;
--- src/test/java/org/mynah/brew/util/ConstantsTest.java
+++ src/test/java/org/mynah/brew/util/ConstantsTest.java
@@ -6,14 +6,14 @@ import org.mynah.brew.util.Constants;
 
 public class ConstantsTest {
 
-    public static final String CONTEXT = "classpath:mist-context.xml";
-    public static final String SERVLET_CONTEXT = "classpath:mist-servlet-context.xml";
+    public static final String CONTEXT = "classpath:context.xml";
+    public static final String SERVLET_CONTEXT = "classpath:servlet-context.xml";
     public static final String WEB_ROOT = "src/main/webapp";
 
     @Test
     public void test() {
         // new Constants();
-        assertEquals("Mist", Constants.PROJECT_NAME);
+        assertEquals("Brew", Constants.PROJECT_NAME);
     }
 
 }
