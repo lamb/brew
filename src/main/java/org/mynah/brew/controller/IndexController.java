b2541e8e3f2022e89094743f424cf205bafeb8ab
Lamb <lambgao@me.com>
2013-01-29 18:09:50 +0800

    index

--- src/main/java/org/mynah/brew/controller/IndexController.java
+++ src/main/java/org/mynah/brew/controller/IndexController.java
@@ -1,14 +1,41 @@
 package org.mynah.brew.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.mynah.brew.service.UserService;
+import org.mynah.brew.util.Constants;
+import org.mynah.brew.util.CookieUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public String index(HttpServletRequest request) {
+        String view = "index/page";
+        HttpSession session = request.getSession();
+        Object object = session.getAttribute(Constants.SESSION_USER);
+        if (null == object) {
+            Cookie[] cookies = request.getCookies();
+            String su = CookieUtil.getValue(cookies, Constants.COOKIE_SU);
+            if (null != su) {
+                String[] values = su.split(":");
+                String email = values[0];
+                String password = values[1];
+                if (userService.verifyPassword(email, password)) {
+                    session.setAttribute(Constants.SESSION_USER, userService.get(email));
+                    view = "redirect:/home";
+                }
+            }
+        }
+        return view;
     }
 
 }
