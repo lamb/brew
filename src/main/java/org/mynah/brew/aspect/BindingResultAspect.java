06a74a7036bbdde75f34e9528ef09f029cdfe349
Lamb <lambgao@me.com>
2013-02-10 19:05:26 +0800

    //TODO 吃饭先...

--- src/main/java/org/mynah/brew/aspect/BindingResultAspect.java
+++ src/main/java/org/mynah/brew/aspect/BindingResultAspect.java
@@ -1,11 +1,15 @@
 package org.mynah.brew.aspect;
 
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
 
 @Aspect
 public class BindingResultAspect {
@@ -14,13 +18,17 @@ public class BindingResultAspect {
     protected final Log logger = LogFactory.getLog(getClass());
 
     @Around("execution(* org.mynah.brew.controller..*.*(..)) && args (..,result)")
-    public String valid(ProceedingJoinPoint pjp, BindingResult result) throws Throwable {
+    public Object valid(ProceedingJoinPoint pjp, BindingResult result) throws Throwable {
         logger.debug("result=" + result);
-        String value = null;
+        Object value = null;
         if (result.hasErrors()) {
+            ModelAndView mav = new ModelAndView();
+            Map<String, Object> model = mav.getModel();
+            model.put("result", result);
+            mav.getView();//TODO 吃饭先...
             value = "result";
         } else {
-            value = (String) pjp.proceed();
+            value = pjp.proceed();
         }
         return value;
     }
